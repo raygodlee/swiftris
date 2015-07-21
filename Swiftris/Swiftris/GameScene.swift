b824f7ceb02807f32e3da82c226299d0b2b30e38
vicxia <shf.xia@gmail.com>
2015-07-21 09:42:21 +0800

    界面更新定时

--- Swiftris/Swiftris/GameScene.swift
+++ Swiftris/Swiftris/GameScene.swift
@@ -8,7 +8,13 @@
 
 import SpriteKit
 
+let TickLengthLevelOne = NSTimeInterval(600)
+
 class GameScene: SKScene {
+    
+    var tick: (()->())?
+    var tickLengthMillis = TickLengthLevelOne
+    var lastTick: NSDate?
     
     required init(coder aDecoder: NSCoder)
     {
@@ -29,5 +35,23 @@ class GameScene: SKScene {
     
     override func update(currentTime: CFTimeInterval) {
         /* Called before each frame is rendered */
+        if lastTick == nil {
+            return
+        }
+        
+        var timePassed = lastTick!.timeIntervalSinceNow * -1000.0
+        if timePassed > tickLengthMillis
+        {
+            lastTick = NSDate()
+            tick?()
+        }
+    }
+    
+    func startTicking() {
+        lastTick = NSDate()
+    }
+    
+    func stopTicking() {
+        lastTick = nil
     }
 }
